54ff5b72e17ef055f53559f983bbe26ac80eec95
bduong0929 <bao.duong@revature.com>
2023-06-23 17:07:46 -0400

    Added Spring Boot

---
+++ src/test/java/com/revature/strawberry/strawberry/StrawberryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.strawberry.strawberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrawberryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
